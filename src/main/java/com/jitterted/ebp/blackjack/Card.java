95da3ce9e939e0a3c11a61b145addaf6785cc1a7
Cristian Ricardo Gulisano <cgulisano@salesforce.com>
2022-09-28 14:44:44 -0300

    Initial commit

---
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -0,0 +1,71 @@
+package com.jitterted.ebp.blackjack;
+
+import org.fusesource.jansi.Ansi;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class Card {
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public int rankValue() {
+        return rank.value();
+    }
+
+    public static String display(Card card) {
+        String[] lines = new String[7];
+        lines[0] = "┌─────────┐";
+        lines[1] = String.format("│%s%s       │", card.rank().display(), card.rank() == Rank.TEN ? "" : " ");
+        lines[2] = "│         │";
+        lines[3] = String.format("│    %s    │", card.suit().symbol());
+        lines[4] = "│         │";
+        lines[5] = String.format("│       %s%s│", card.rank() == Rank.TEN ? "" : " ", card.rank().display());
+        lines[6] = "└─────────┘";
+
+        Ansi.Color cardColor = card.suit().isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
+        return ansi()
+                .fg(cardColor).toString()
+                + String.join(ansi().cursorDown(1)
+                                    .cursorLeft(11)
+                                    .toString(), lines);
+    }
+
+    private Suit suit() {
+        return suit;
+    }
+
+    private Rank rank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Card {" +
+                "suit=" + suit +
+                ", rank=" + rank +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (!suit.equals(card.suit)) return false;
+        return rank.equals(card.rank);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + rank.hashCode();
+        return result;
+    }
+}
---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,22 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CardDisplayTest {
+
+    @Test
+    void displayTenAsString() {
+        Card card = new Card(Suit.SPADES, Rank.TEN);
+        assertThat(Card.display(card))
+                .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+
+    @Test
+    void displayNonTenAsString() {
+        Card card = new Card(Suit.SPADES, Rank.EIGHT);
+        assertThat(Card.display(card))
+                .isEqualTo("[30m┌─────────┐[1B[11D│8        │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│        8│[1B[11D└─────────┘");
+    }
+}
